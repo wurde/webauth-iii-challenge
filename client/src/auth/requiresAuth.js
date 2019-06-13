7ea149dc1517409e016d42619d664dd337a43f36
Andy Bettisworth <andy@accreu.com>
2019-06-13 17:27:17 -0500

    Improve styles on authentication page

--- client/src/auth/requiresAuth.js
+++ client/src/auth/requiresAuth.js
@@ -3,6 +3,7 @@
  */
 
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import axios from 'axios'
 
 /**
@@ -19,9 +20,19 @@ axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080'
 function requiresAuth(Component) {
   return class Authenticated extends Component {
     render() {
-      const notLoggedIn = <div>Please login to see our users</div>
-
-      return <>{token ? <Component {...this.props} /> : notLoggedIn}</>
+      if (token) {
+        return <Component {...this.props} />
+      } else {
+        return (
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-10 col-md-8 col-lg-5" style={{ marginTop: '80px' }}>
+                <p>Requires authentication: <NavLink to="/users/signin">Sign in</NavLink></p>
+              </div>
+            </div>
+          </div>
+        )
+      }
     }
   }
 }
